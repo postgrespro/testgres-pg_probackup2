59f4e3c9f63a8f258cf50cacb27a20ff96cc3c0b
Victoria Shepard <5807469+demonolock@users.noreply.github.com>
2026-05-26 13:04:39 +0200

    Error with ARCHIVE_COMPRESSION zstd and lz4 (#15)
    * PBCKP-3357 add import zstd, lz4
    
    * Up version 0.8.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    version='0.7.0',
+    version='0.8.0',
     name='testgres_pg_probackup2',
     packages=['pg_probackup2', 'pg_probackup2.storage'],
     description='Plugin for testgres that manages pg_probackup2',
@@ -14,5 +14,5 @@ setup(
     author='Postgres Professional',
     author_email='testgres@postgrespro.ru',
     keywords=['pg_probackup', 'testing', 'testgres'],
-    install_requires=['testgres>=1.9.2']
+    install_requires=['testgres>=1.9.2', 'zstd', 'lz4']
 )
